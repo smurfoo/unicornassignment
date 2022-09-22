5503399c0c507e8cb49779e8d325e0975ecbb3b7
smurfoo <ilyasguleed6@gmail.com>
2022-09-22 13:36:18 -0600

    update

--- Program.cs
+++ Program.cs
@@ -19,13 +19,12 @@ namespace unicornassignment
         {
             // declare variables
             string donorName = "", unicornName = "";
-            char options, optionTotal = ' ', penOptions = ' ', gateOptions = ' ', gateBoolean = ' ', paintIf = ' ', mealBoolean = ' ', mealUpgrade = ' ';
+            char options, gateOptions = ' ', gatePaint = ' ', mealUpgrade = ' ', mealPriceOption = ' ', paintOption = ' ';
             double monthAmount = 0, months = 0, donationAmount = 0, wallWidth = 0, backWallWidth = 0, gateWidth = 0, gateHeight = 0,
-                wallPrice = 0, wallArea = 0, gateArea = 0, gatePrice = 0, mealPrice = 0, area = 0;
+                wallPrice = 0, wallArea = 0, gateArea = 0, gatePrice = 0, mealPrice = 0, backWallArea = 0, backGateArea = 0, paintPrice = 0, totalPrice = 0, gateSquareFeet = 0;
             // inputs
             // menu donation type
             Console.WriteLine("***** Welcome to the Unicorn Rescue Society Sponsor Estimator *****");
-
             Console.Write("What is the sponsor's name?");
             donorName = Console.ReadLine();
             Console.Write("What would you like to call the unicorn?");
@@ -37,7 +36,6 @@ namespace unicornassignment
             Console.WriteLine("\tO - One Time");
             Console.Write("Option: ");
             options = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-
             // options
             switch (options)
             {
@@ -66,7 +64,6 @@ namespace unicornassignment
                     Console.WriteLine("Invalid Selection");
                     break;
             }
-
             // menu pen accomodations
             Console.WriteLine("***** Pen Accommodations *****");
             Console.Write("What is the width of the 2 walls (in feet)? ");
@@ -77,24 +74,23 @@ namespace unicornassignment
             gateWidth = double.Parse(Console.ReadLine());
             Console.Write("What is the height of the gate? ");
             gateHeight = double.Parse(Console.ReadLine());
-
-
-
             // wall price calculations
-            // length x width  = square feet
-            if (gateArea < backWallWidth)
+
+            if (gateWidth < backWallWidth)
             {
                 gateArea = ((backWallWidth - gateWidth) * (12 * 4));
             }
             else
             {
                 Console.WriteLine("Gate cannot be wider than back wall width");
-                Environment.Exit(0);    
+                Environment.Exit(0);
             }
-          
-          
-           
-
+            wallArea = ((wallWidth * 2) * (12 * 4));
+            backWallArea = ((backWallWidth) * (12 * 4));
+            backGateArea = ((backWallWidth - gateWidth) * (12 * 4));
+            gatePrice = backWallArea + backGateArea;
+            wallPrice = wallArea + backWallArea + backGateArea;
+            gateSquareFeet = ((gateWidth + gateHeight) * 4);
             // gate menu
             Console.WriteLine("***** Gate Style ******");
             Console.WriteLine("Available gates (prices per square foot):");
@@ -103,102 +99,109 @@ namespace unicornassignment
             Console.WriteLine("\tG- Gold ($15) ");
             Console.Write("Option: ");
             gateOptions = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-
             // gate options
 
             switch (gateOptions)
             {
                 case 'W':
-
+                    gatePrice = gateSquareFeet * 3;
 
                     break;
                 case 'S':
+                    gatePrice = gateSquareFeet * 8;
                     break;
                 case 'G':
+                    gatePrice = gateSquareFeet * 15;
                     break;
             }
             // (must use if and switch, if will be used when asking to change gate paint or a meal upgrade yes or no
             // gate paint change option
             Console.Write("Would you like to change the gate paint (Y/N)?");
-            gateBoolean = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-
-            switch (gateBoolean)
+            gatePaint = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
+            switch (gatePaint)
             {
                 case 'Y':
                     Console.WriteLine("Available paints");
                     Console.WriteLine("\tM - Mood: Changes colour based on mood ($200)");
                     Console.WriteLine("\tA - Magic: Changes colour several times a day ($300)");
                     Console.WriteLine("\tR - Reflective: Reflects like a mirror ($150)");
+                    Console.Write("Option: ");
+                    paintOption = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
+                    switch (paintOption)
+                    {
+                        case 'M':
+                            paintPrice = paintPrice + 200;
+                            break;
+                        case 'A':
+                            paintPrice = paintPrice + 300;
+                            break;
+                        case 'R':
+                            paintPrice = paintPrice + 150;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid selection, please try again.");
+                            Environment.Exit(0);
+                            break;
+
+                    }
+            
+            default:
+                    Console.WriteLine("Invalid Selection");
+                    Environment.Exit(0);
                     break;
 
+        }
 
-
-
-            }
-            // option if structure
-            Console.Write("Option: ");
-            paintIf = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-            if (paintIf == 'M')
-            {
-                gatePrice += 200;
-            }
-            else if (paintIf == 'A')
-            {
-                gatePrice += 300;
-
-            }
-            else
-            {
-                gatePrice += 150;
-            }
             // meal upgrade menu
             Console.WriteLine("***** Meal Upgrade *****");
             Console.Write("Would you like a meal upgrade (Y/N)? ");
-            mealBoolean = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-            // meal upgrade if structure
-            if (mealBoolean == 'Y')
-            {
-                Console.WriteLine("Available meal upgrades:");
-                Console.WriteLine("\tR - Add rainbow cookie treats ($1000)");
-                Console.WriteLine("\tS - Special appetizers ($500)");
-                mealUpgrade = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
-                switch (mealUpgrade)
-                {
-                    case 'R':
-                        mealPrice += 1000;
-                        break;
-                    case 'S':
-                       
-                }
-
-            }
-    
-            
-            /*      switch (gateBoolean)
+            mealUpgrade = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
+            // meal upgrade switch
+            switch (mealUpgrade)
             {
                 case 'Y':
-                Console.WriteLine("Available meal upgrades:");
-                Console.WriteLine("\tR - Add rainbow cookie treats ($1000)");
-                Console.WriteLine("\tS - Special appetizers ($500)");
+                    Console.WriteLine("Available meal upgrades:");
+                    Console.WriteLine("\tR - Add rainbow cookie treats ($1000)");
+                    Console.WriteLine("\tS - Special appetizers ($500)");
+                    mealPriceOption = char.Parse(Console.ReadLine().ToUpper().Substring(0, 1));
+                    switch (mealPriceOption)
+                    {
+                        case 'R':
+                            mealPrice = mealPrice + 1000;
+                            break;
+                        case 'S':
+                            mealPrice = mealPrice + 500;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Selection");
+                            Environment.Exit(0);
+                            break;
+                    }
                     break;
-
-
-
-
-
             }
 
-*/
-
-
-
-
-
-            Console.WriteLine($"Wall Price is now {wallPrice} for {squareFeet} ");
-
-
-
+            // total price
+            totalPrice = (donationAmount) + (wallPrice) + (gatePrice) + (paintPrice) + (mealPrice) + (mealPrice);
+            // summary
+            Console.WriteLine("***** Summary *****");
+            Console.WriteLine($" Donor                       {donorName}");
+            Console.WriteLine($" Unicorn Name                       {unicornName}");
+            Console.WriteLine($" Donation Amount                       {donationAmount}");
+            Console.WriteLine($" Wall Cost                       {wallPrice}");
+            Console.WriteLine($" Gate Cost                       {gatePrice}");
+            Console.WriteLine($" Gate Paint Cost                       {paintPrice}");
+            Console.WriteLine($" Meal Upgrade                      {mealPrice}");
+            Console.WriteLine($"{donorName}, the total cost to sponsor {unicornName} is {totalPrice}");
 
         }
     }
 }
+
+
+
+
+
+
+
+
+
